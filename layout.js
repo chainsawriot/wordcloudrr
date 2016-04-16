d8b9a6951b8775f52d079e6c6f703448d8ee737e
chainsawriot <chainsawtiney@gmail.com>
2016-04-17 01:05:29 +0800

    gengraph.R proof of concept

--- layout.js
+++ layout.js
@@ -1,29 +1,26 @@
 window.onload = function () {
+    var wordcloudrr = JSON.parse(data)
+    var wordcloudrr_list = []
+    for (var row in wordcloudrr['data']) {
+	wordcloudrr_list.push([wordcloudrr['data'][row]['word'], wordcloudrr['data'][row]['size']])
+    }
     options= {
-	list : [["本港", "126.232"],
-		["提升", "120.612"],
-		["權力", "113.028"],
-		["特權", "113.028"],
-		["張超雄", "111.613"],
-		["六四", "26.909"],
-		["可持續", "26.909"],
-		["基建", "26.909"],
-		["基本法", "26.909"]],
+	list : wordcloudrr_list,
 	rotateRatio: 0,
 	shape: "triangle",
-	color: function (word, weight) {
-	    var floatweight = parseFloat(weight);
-	    if ( floatweight > 100 && floatweight <= 130 ) {
-		return('#00441b');
-	    } else if (floatweight > 80 && floatweight <= 100) {
-		return('#006d2c');
-	    } else if (floatweight > 60 && floatweight <= 80) {
-		return('#238b45');
-	    } else if (floatweight > 40 && floatweight <= 60) {
-		return('#41ab5d');
-	    } else {
-		return('#74c476');
-	    }}
-    };
-    WordCloud(document.getElementById('wordcloudrr'), options);
-};
+	// color: function (word, weight) {
+	//     var floatweight = parseFloat(weight);
+	//     if ( floatweight > 100 && floatweight <= 130 ) {
+	// 	return('#00441b');
+	//     } else if (floatweight > 80 && floatweight <= 100) {
+	// 	return('#006d2c');
+	//     } else if (floatweight > 60 && floatweight <= 80) {
+	// 	return('#238b45');
+	//     } else if (floatweight > 40 && floatweight <= 60) {
+	// 	return('#41ab5d');
+	//     } else {
+	// 	return('#74c476');
+	//     }}
+    }
+    WordCloud(document.getElementById('wordcloudrr'), options)
+}
